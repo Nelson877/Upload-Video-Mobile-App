e56600acfd0421a8fcedb6ce56e0dd2f6c3548de
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2025-06-29 20:36:12 +0000

    Started the upload video app

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+const TabsLayout = () => {
+  return (
+    <View>
+      <Text>TabsLayout</Text>
+    </View>
+  )
+}
+
+export default TabsLayout
+
+const styles = StyleSheet.create({})
