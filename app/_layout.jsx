69a899eed0b5db0bf4438f154434402b48aa9bcb
Nelson877 <66440163+Nelson877@users.noreply.github.com>
2025-07-01 11:31:25 +0000

    Done with sign-up and sign-in page

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -3,6 +3,8 @@ import { SplashScreen, Stack } from "expo-router";
 import React, { useEffect } from "react";
 import "../global.css";
 
+import GlobalProvider from "../context/GlobalProvider";
+
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -38,9 +40,14 @@ const RootLayout = () => {
   }
 
   return (
-    <Stack>
-      <Stack.Screen name='index' options={{ headerShown: false }} />
-    </Stack>
+     <GlobalProvider>
+      <Stack>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="(auth)" options={{ headerShown: false }} />
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        
+      </Stack>
+    </GlobalProvider>
   );
 };
 
